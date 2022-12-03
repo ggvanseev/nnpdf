355bb28672efa9af2ce6c74d5725172a8bc95691
t7phy <t7phy.in@gmail.com>
2022-12-03 03:08:58 +0100

    cms data and kinematics

--- buildmaster/CMS_Jet_13TeV_r07/filter.py
+++ buildmaster/CMS_Jet_13TeV_r07/filter.py
@@ -4,3 +4,52 @@ import yaml
 def processData():
     with open('metadata.yaml', 'r') as file:
         metadata = yaml.safe_load(file)
+    
+    version = metadata['hepdata']['version']
+    tables = metadata['hepdata']['tables']
+
+    data_central = []
+    kin = []
+    error = []
+    error_nuc = []
+
+    for i in tables:
+        if i == 1:
+            y_min = 0
+            y_max = 0.5
+        elif i == 2:
+            y_min = 0.5
+            y_max = 1
+        elif i == 3:
+            y_min = 1
+            y_max = 1.5
+        elif i == 4:
+            y_min = 1.5
+            y_max = 2
+        y_central = float((y_min+y_max)/2)
+        hepdata_tables="rawdata/ak7_xsec_ybin"+str(i)+".yaml"
+        with open(hepdata_tables, 'r') as file:
+            input = yaml.safe_load(file)
+
+        values = input['dependent_variables'][0]['values']
+        sqrt_s = 13000
+
+        for j in range(len(values)):
+            data_central_value = input['dependent_variables'][0]['values'][j]['value']
+            data_central.append(data_central_value)
+            pT_min = input['independent_variables'][0]['values'][j]['low']
+            pT_max = input['independent_variables'][0]['values'][j]['high']
+            pT_central = float((pT_min+pT_max)/2)
+            kin_value = {'sqrt_s': {'min': None, 'mid': sqrt_s, 'max': None}, 'pT': {'min': pT_min, 'mid': pT_central, 'max': pT_max}, 'y': {'min': y_min, 'mid': y_central, 'max': y_max}}
+            kin.append(kin_value)
+            
+    data_central_yaml = {'data_central': data_central}
+    kinematics_yaml = {'bins': kin}
+
+    with open('data.yaml', 'w') as file:
+        yaml.dump(data_central_yaml, file, sort_keys=False)
+
+    with open('kinematics.yaml', 'w') as file:
+        yaml.dump(kinematics_yaml, file, sort_keys=False)
+
+processData()
